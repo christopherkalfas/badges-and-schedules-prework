7b1826988db55d7e3f1c4a50470a0a8d09594d9d
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 17:44:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,11 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end 
 
-def batch_badge_creator(attendees)
-  attendees = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-    attendees.each do |messages|
-     return "Hello, my name is #{messages}."
-    end 
+def batch_badge_creator(names)
+  names = [ "Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+  names.each do
+    "Hello, my name is #{names}"
+  end
    
 end 
